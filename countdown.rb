fa6b3871edacaa20636a0ffbf8b15eb935c3feab
Andrew Cheon <cheonandrew113@gmail.com>
2019-07-17 17:20:11 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,29 @@
 #write your code here
+def countdown(x)
+    while x > 0
+      puts "#{x} SECOND(S)!"
+      sleep 1
+      x -= 1
+    break if x == 0
+    end	
 
-def countdown
+    return "HAPPY NEW YEAR!"
 end
+
+
+ def countdown_with_sleep(num) 
+   one_sec = sleep 1 
+   while num > 0
+     puts "#{num}" 
+     num -= one_sec
+   end 
+   return "Goodnight"
+ end 
+
+ def countdown_with_sleep(num)
+   1.upto(num) do |num| 
+     puts num
+     sleep 1 
+   end
+   return "Goodnight"
+ end 
